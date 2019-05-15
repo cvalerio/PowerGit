db303a6964516335b0fbc965dab8d5ccd1bc8dc5
Claudio Valerio <info@claudiovalerio.it>
2019-05-15 17:30:00 +0200

    It seems to work

--- powergit.psm1
+++ powergit.psm1
@@ -1,8 +1,31 @@
 ﻿function Clear-Local-Git-Branches {
-    echo "This will delete all these local branches:"
-    git --no-pager branch -l | where { !($_ -match "master") } 
+    $command = "git --no-pager branch -l --merged"
+    $branches = Invoke-Expression $command | where { !($_ -match "master") } 
+    if($branches.Count -eq 0)
+    {
+        echo "No local merged branches found"
+        return
+    }
+    echo "This will delete all these local merged branches:"
+    echo $branches
     $confirm = Read-Host -Prompt "Do you wish to continue? [Y/n]"
     if ($confirm.ToUpperInvariant() -eq "Y") {
-        git branch -l | where { !($_ -match "master") } | foreach { git branch -D "$($_.Trim())" }
+        Invoke-Expression $command | where { !($_ -match "master") } | foreach { git branch -D "$($_.Trim())" }
+    }
+}
+
+function Clear-Remote-Git-Branches {
+    $command = "git --no-pager branch -r --merged"
+    $branches = Invoke-Expression $command | where { !($_ -match "master") } 
+    if($branches.Count -eq 0)
+    {
+        echo "No remote merged branches found"
+        return
+    }
+    echo "This will delete all these local merged branches:"
+    echo $branches
+    $confirm = Read-Host -Prompt "Do you wish to continue? [Y/n]"
+    if ($confirm.ToUpperInvariant() -eq "Y") {
+        Invoke-Expression $command | where { !($_ -match "master") } | foreach { git push --delete origin "$($_.Split('/')[1])" }
     }
 }
