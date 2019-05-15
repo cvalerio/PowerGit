22d65652a8580c090427e59f4331b5cbf263492a
Claudio Valerio <info@claudiovalerio.it>
2019-05-15 17:08:03 +0200

    I'm working

--- powergit.psm1
+++ powergit.psm1
@@ -1,3 +1,8 @@
 ﻿function Clear-Local-Git-Branches {
-    git branch -l | where { !($_ -match "master") } | foreach { git branch -D "$($_.Trim())" }
+    echo "This will delete all these local branches:"
+    git --no-pager branch -l | where { !($_ -match "master") } 
+    $confirm = Read-Host -Prompt "Do you wish to continue? [Y/n]"
+    if ($confirm.ToUpperInvariant() -eq "Y") {
+        git branch -l | where { !($_ -match "master") } | foreach { git branch -D "$($_.Trim())" }
+    }
 }
